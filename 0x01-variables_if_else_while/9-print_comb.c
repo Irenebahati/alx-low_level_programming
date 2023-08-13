8bffd686cf557090343f221701c7ed5653a9c0bf
Irenebahati <ngumbaoirene174@gmail.com>
2023-08-13 14:27:53 -0700

    prints all possible combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 0; z < 10; z++)
+	{
+		putchar(z + '0');
+		if (z < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
