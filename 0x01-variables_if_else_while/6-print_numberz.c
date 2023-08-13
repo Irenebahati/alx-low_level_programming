479314532d6806a49378f3bd9dc71d4b596f93d2
Irenebahati <ngumbaoirene174@gmail.com>
2023-08-13 13:46:38 -0700

    Print numberzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Beginning point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int Z = 0;
+
+	while
+		(Z < 10) {
+			putchar(Z + '0');
+			Z++;
+		}
+	putchar('\n');
+	return (0);
+}
