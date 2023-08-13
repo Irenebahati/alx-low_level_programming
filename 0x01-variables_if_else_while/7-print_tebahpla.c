6f86143244881911988a4b00cc25b7ac0d0aa218
Irenebahati <ngumbaoirene174@gmail.com>
2023-08-13 14:01:22 -0700

    Print alphabets from z to a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Beginning point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char A = 'z';
+
+	while
+		(A >= 'a') {
+			putchar(A);
+			A--;
+		}
+	putchar('\n');
+	return (0);
+}
