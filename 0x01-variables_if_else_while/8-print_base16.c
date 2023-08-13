4dd4390d63884b47ef546d045efb1d53c70418e6
Irenebahati <ngumbaoirene174@gmail.com>
2023-08-13 14:11:18 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Beginning of program
+ * Return: 0
+ */
+
+int main(void)
+{
+	char A = 'a';
+	int B = 0;
+
+	while
+		(B < 10) {
+			putchar(B + '0');
+			B++;
+		}
+	while
+		(A <= 'f') {
+			putchar(A);
+			A++;
+		}
+	putchar('\n');
+	return (0);
+}
