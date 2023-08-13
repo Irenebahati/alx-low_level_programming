1f418d096ec4a59174599483a0d3f02b05ace031
Irenebahati <ngumbaoirene174@gmail.com>
2023-08-13 07:24:51 -0700

    Print alpabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - The program begins here
+ * Return: 0
+ */
+
+int main(void)
+{
+	char A = 'a';
+
+	while
+		(A <= 'z') {
+			putchar (A);
+			A++;
+		}
+	putchar('\n');
+	return (0);
+}
