7293eb25c39a6b2d3d15f3d7088304bd1c04ff79
Irenebahati <ngumbaoirene174@gmail.com>
2023-10-20 04:36:25 -0700

    elf header file update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -276,7 +276,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	int o, r;
 
 	o = open(argv[1], O_RDONLY);
-	if (p == -1)
+	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
