e46e4fe14c732b66150a94e66304a1d09311dfb0
Irenebahati <ngumbaoirene174@gmail.com>
2023-11-27 23:24:09 +0300

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,12 +3,12 @@
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments passed to the program
- * @argv: array of arguments
+ * @argv: array of argument
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int fd_a, fd_b, a, x, y;
+	int fd_r, fd_w, r, a, b;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
@@ -16,36 +16,36 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_a = open(argv[1], O_RDONLY);
-	if (fd_a < 0)
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_b = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((a = read(fd_a, buf, BUFSIZ)) > 0)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		if (fd_b < 0 || write(fd_b, buf, a) != a)
+		if (fd_w < 0 || write(fd_w, buf, r) != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_a);
+			close(fd_r);
 			exit(99);
 		}
 	}
-	if (a < 0)
+	if (r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	x = close(fd_a);
-	y = close(fd_b);
-	if (x < 0 || y < 0)
+	a = close(fd_r);
+	b = close(fd_w);
+	if (a < 0 || b < 0)
 	{
-		if (x < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_a);
-		if (y < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_b);
-			exit(100);
-																						}
+	if (a < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+	if (b < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
+	}
 	return (0);
 }
